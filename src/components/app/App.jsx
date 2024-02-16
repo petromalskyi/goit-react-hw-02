1a9bc72e6279a636a573f68b8bcf4e0bdb4a4e67
petromalskyi <113782385+petromalskyi@users.noreply.github.com>
2024-02-16 15:04:08 +0200

    finish hw-2

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import css from './App.module.css';
 import Description from '../description/Description';
 import Options from '../options/Options';
 import Feedback from '../feedback/Feedback';
 
+const getStartOption = () => {
+  const savedLocalStorage = window.localStorage.getItem('saved-clicks');
+  return savedLocalStorage
+    ? JSON.parse(savedLocalStorage)
+    : {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      };
+};
+
 export default function App() {
-  const [values, setValues] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [values, setValues] = useState(getStartOption);
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-clicks', JSON.stringify(values));
+  }, [values]);
 
   const updateFeedback = feedbackType => {
     setValues({
@@ -17,6 +28,7 @@ export default function App() {
       [feedbackType]: values[feedbackType] + 1,
     });
   };
+
   const reset = () => {
     setValues({
       good: 0,
